36b0e70dc77d487bf6f126723d632e09420c6706
afwteas <afwteas@phys.uoa.gr>
2019-02-17 21:33:15 +0200

    update

--- Mod2Lab/app/menuController.js
+++ Mod2Lab/app/menuController.js
@@ -2,8 +2,16 @@ app.controller('menuController', [
     '$scope',
     function($scope){
         $scope.model = {title: 'Our Menu'};
-        $scope.changeMainDish = function (item) {
-            $scope.model.mainDish = item;
+        $scope.changeMainDish = function (name,price) {
+            $scope.model.mainDish= {'name':name,
+                'price':price};
+           
+            
         }
+        $scope.$watch('model.mainDish', function (newValue, oldValue) {
+            if (newValue === 'BBQ Chicken Pizza') {
+                alert('You have selected the BBQ Chicken Pizza!');
+            }
+        });
     }
 ]);
